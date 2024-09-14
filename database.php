72b617e86bc274f5ba658e8390b66334f6055bb7
Sainn <32024592+Sainnnx@users.noreply.github.com>
2024-09-14 19:47:08 +0700

    Update database.php 3

--- database.php
+++ database.php
@@ -1,3 +1,15 @@
 <?php
-$con = mysqli_connect("sql12.freesqldatabase.com","sql12731363","PZYTnew3CQ","sql12731363");
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+
+$con = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "Connected successfully!";
+}
 ?>
